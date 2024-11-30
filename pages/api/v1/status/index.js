1c259652d5103c3168f4fdd9f1c6603f86cec778
Dante <beyondmind9@gmail.com>
2024-11-30 01:29:32 -0300

    finish api/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,31 @@
-import database from "infra/database.js";
+import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("select 1 + 1 as sum");
-  console.log(result.rows);
-  response
-    .status(200)
-    .json({ chave: "alunos do curso.dev são pessoas acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseMaxConnections = await database.query("show max_connections;");
+  const maxConnections = databaseMaxConnections.rows[0].max_connections;
+
+  const databaseVersion = await database.query("show server_version;");
+  const version = databaseVersion.rows[0].server_version;
+
+  const dataBaseName = process.env.POSTGRES_DB;
+  const databaseConnections = await database.query({
+    text: "select count(*)::Int from pg_stat_activity where datname = $1;",
+    values: [dataBaseName],
+  });
+  const connections = databaseConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: version,
+        connections: connections,
+        max_connections: parseInt(maxConnections),
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,14 @@
 test("Get to api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
 
+  const responseBody = await response.json();
+  const parsedDate = new Date(responseBody.updated_at).toISOString();
+  console.log(responseBody);
+
+  const database = responseBody.dependencies.database;
   expect(response.status).toBe(200);
+  expect(responseBody.updated_at).toEqual(parsedDate);
+  expect(database.max_connections).toBeGreaterThan(0);
+  expect(database.connections).toEqual(1);
+  expect(database.version).toEqual("16.6");
 });

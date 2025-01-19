d7681e1a6cebc24c6eac8f64e8ba17106527a849
Dante <beyondmind9@gmail.com>
2025-01-18 23:47:48 -0300

    test: use `orchestrator.clearDatabase()` for tests

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -3,6 +3,7 @@ import orchestrator from "tests/orchestrator";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
 });
 
 async function cleanDatabase() {
